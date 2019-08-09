4fd541475c283bdd5309a98aa05c40c9c8df97a4
portree_kid <keith.paterson@gmx.de>
2019-08-09 09:48:02 +0200

    Initial Version

---
+++ groundnet_checker/src/main/java/de/keithpaterson/groundnet_checker/GroundNetTest.java
@@ -0,0 +1,97 @@
+package de.keithpaterson.groundnet_checker;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.jgrapht.Graph;
+import org.jgrapht.GraphPath;
+import org.jgrapht.Graphs;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.graph.DefaultEdge;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+import org.w3c.dom.Element;
+
+public class GroundNetTest {
+
+	private File projectBaseDir;
+
+	@ParameterizedTest(name="Unconnected nodes : {0}")
+	@ArgumentsSource(FileProvider.class)
+	public void testUnconnected(String f, Graph<Element, DefaultEdge> loadGraph) {
+		try {
+			if(loadGraph.edgeSet().size()==0)
+				return;
+			List<Element> unconnected = loadGraph.vertexSet().stream()
+					.filter(p ->  Graphs.neighborListOf(loadGraph, p).size()== 0 ).collect(Collectors.toList());
+			assertEquals(0,unconnected.size(),"There are unconnected nodes in " + f);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@ParameterizedTest(name="Runway routes {0}")
+	@ArgumentsSource(FileProvider.class)
+	public void testRunwayRoutes(String f, Graph<Element, DefaultEdge> loadGraph) {
+		try {
+			List<Element> runwayEnds = loadGraph.vertexSet().stream()
+					.filter(p ->  Graphs.neighborListOf(loadGraph, p).size() == 1 )
+					.filter(p -> p.getAttribute("isOnRunway").equals("1") )
+					.collect(Collectors.toList());
+			List<Element> parkingEnds = loadGraph.vertexSet().stream()
+					.filter(p -> Graphs.neighborListOf(loadGraph, p).size() == 1 )
+					.filter(p -> p.hasAttribute("type") )
+					.collect(Collectors.toList());
+			for (Element parkingNode : parkingEnds) {
+				for (Element runwayNode : runwayEnds) {
+					GraphPath<Element, DefaultEdge> pathBetween = DijkstraShortestPath.findPathBetween(loadGraph, parkingNode, runwayNode);
+					assertNotNull( pathBetween, "No route between Node "+ parkingNode.getAttribute("name") + " (" + parkingNode.getAttribute("index") + ") Node " + parkingNode.getAttribute("name") + " (" + runwayNode.getAttribute("index") + ")");
+//					System.out.println("Length " + pathBetween.getLength());
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static class FileProvider implements ArgumentsProvider {
+
+		private static List<Arguments> files = fileProvider();
+
+		private static List<Arguments> fileProvider() {
+//			File projectBaseDir = new File("C:\\Users\\keith.paterson\\Documents\\FlightGear\\main");
+			File projectBaseDir = new File(".");
+			assertNotNull(projectBaseDir);
+			assertTrue(projectBaseDir.exists());
+			try {
+				return Files.walk(projectBaseDir.toPath()).filter(p -> Files.isRegularFile(p))
+						.filter(p -> !p.getFileName().toString().equals("pom.xml"))
+						.filter(p -> p.getFileName().toString().matches("[a-zA-Z0-9]*\\.(groundnet)\\.xml"))
+						.map(p -> new Object[]{p.getFileName().toString(),new GroundnetLoader().loadGraph(p.toFile())})
+						.map(Arguments::of).collect(Collectors.toList());
+				
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+			return null;
+		}
+
+		@Override
+		public Stream<? extends Arguments> provideArguments(ExtensionContext context) throws Exception {
+			return files.stream();
+		}
+	}
+
+}
---
+++ groundnet_checker/src/main/java/de/keithpaterson/groundnet_checker/GroundnetLoader.java
@@ -0,0 +1,169 @@
+package de.keithpaterson.groundnet_checker;
+
+import java.io.File;
+import java.io.StringReader;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
+
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.builder.GraphTypeBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+
+public class GroundnetLoader  {
+
+	private static Pattern COORDINATE_PATTERN = Pattern.compile("([NSEW])([0-9]*)\\s*([.0-9]*)");
+
+	public void process(File f) throws Exception {
+//		File f = new File(PATH, item.getPath() + "/" + item.icao + ".groundnet.xml");
+		if (f.canRead()) {
+			try (Scanner scanner = new Scanner(f, StandardCharsets.UTF_8.toString())) {
+				scanner.useDelimiter("\\A");
+				String content = scanner.hasNext() ? scanner.next() : "";
+				DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+				factory.setNamespaceAware(true);
+				DocumentBuilder builder;
+				Document doc = null;
+				builder = factory.newDocumentBuilder();
+				doc = builder.parse(new InputSource(new StringReader(content)));
+
+				// Create XPathFactory object
+				XPathFactory xpathFactory = XPathFactory.newInstance();
+
+				HashMap<String, Element> nodes = new HashMap<>();
+				// Create XPath object
+				XPath xpath = xpathFactory.newXPath();
+				NodeList parkingNodes = (NodeList) xpath.evaluate("/groundnet/parkingList/Parking", doc,
+						XPathConstants.NODESET);
+				for (int i = 0; i < parkingNodes.getLength(); i++) {
+					Element n = (Element) parkingNodes.item(i);
+					double lat = parse(n.getAttribute("lat"));
+					double lon = parse(n.getAttribute("lon"));
+					nodes.put(n.getAttribute("index"), n);
+				}
+				NodeList taxiNodes = (NodeList) xpath.evaluate("/groundnet/TaxiNodes/node", doc,
+						XPathConstants.NODESET);
+				for (int i = 0; i < taxiNodes.getLength(); i++) {
+					Element n = (Element) taxiNodes.item(i);
+					nodes.put(n.getAttribute("index"), n);
+
+				}
+				NodeList arcNodes = (NodeList) xpath.evaluate("/groundnet/TaxiWaySegments/arc", doc,
+						XPathConstants.NODESET);
+				for (int i = 0; i < arcNodes.getLength(); i++) {
+					Element n = (Element) arcNodes.item(i);
+					Element start = nodes.get(n.getAttribute("begin"));
+					Element end = nodes.get(n.getAttribute("end"));
+					double lat1 = parse(start.getAttribute("lat"));
+					double lon1 = parse(start.getAttribute("lon"));
+					double lat2 = parse(end.getAttribute("lat"));
+					double lon2 = parse(end.getAttribute("lon"));
+					if (n.getAttribute("isPushBackRoute").equals("1")) {
+					} else {
+					}
+				}
+				
+				System.out.println();
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private double parse(String attribute) {
+		Matcher matcher = COORDINATE_PATTERN.matcher(attribute);
+
+		if (matcher.find()) {
+			String sign = matcher.group(1);
+			double degrees = Double.parseDouble(matcher.group(2));
+			double minutes = Double.parseDouble(matcher.group(3));
+//			System.out.println(attribute);
+			return sign.equalsIgnoreCase("N") || sign.equalsIgnoreCase("E") ? (degrees + (minutes / 60))
+					: -(degrees + (minutes / 60));
+		}
+		return 0;
+	}
+
+	public Graph<Element, DefaultEdge> loadGraph(File f)  {
+		
+		Graph<Element, DefaultEdge> g = buildEmptySimpleGraph(); 
+	//		File f = new File(PATH, item.getPath() + "/" + item.icao + ".groundnet.xml");
+			if (f.canRead()) {
+				try (Scanner scanner = new Scanner(f, StandardCharsets.UTF_8.toString())) {
+					scanner.useDelimiter("\\A");
+					String content = scanner.hasNext() ? scanner.next() : "";
+					DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+					factory.setNamespaceAware(true);
+					DocumentBuilder builder;
+					Document doc = null;
+					builder = factory.newDocumentBuilder();
+					doc = builder.parse(new InputSource(new StringReader(content)));
+	
+					// Create XPathFactory object
+					XPathFactory xpathFactory = XPathFactory.newInstance();
+	
+					HashMap<String, Element> nodes = new HashMap<>();
+					// Create XPath object
+					XPath xpath = xpathFactory.newXPath();
+					NodeList parkingNodes = (NodeList) xpath.evaluate("/groundnet/parkingList/Parking", doc,
+							XPathConstants.NODESET);
+					for (int i = 0; i < parkingNodes.getLength(); i++) {
+						Element n = (Element) parkingNodes.item(i);
+						double lat = parse(n.getAttribute("lat"));
+						double lon = parse(n.getAttribute("lon"));
+						nodes.put(n.getAttribute("index"), n);
+	                    g.addVertex(n);
+					}
+					NodeList taxiNodes = (NodeList) xpath.evaluate("/groundnet/TaxiNodes/node", doc,
+							XPathConstants.NODESET);
+					for (int i = 0; i < taxiNodes.getLength(); i++) {
+						Element n = (Element) taxiNodes.item(i);
+						nodes.put(n.getAttribute("index"), n);
+	                    g.addVertex(n);
+					}
+					NodeList arcNodes = (NodeList) xpath.evaluate("/groundnet/TaxiWaySegments/arc", doc,
+							XPathConstants.NODESET);
+					for (int i = 0; i < arcNodes.getLength(); i++) {
+						Element n = (Element) arcNodes.item(i);
+						Element start = nodes.get(n.getAttribute("begin"));
+						Element end = nodes.get(n.getAttribute("end"));
+						g.addEdge(start, end);
+						double lat1 = parse(start.getAttribute("lat"));
+						double lon1 = parse(start.getAttribute("lon"));
+						double lat2 = parse(end.getAttribute("lat"));
+						double lon2 = parse(end.getAttribute("lon"));
+						if (n.getAttribute("isPushBackRoute").equals("1")) {
+						} else {
+						}
+					}
+					
+//					System.out.println();
+	
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			return g;
+		}
+
+	
+	private static Graph<Element, DefaultEdge> buildEmptySimpleGraph()
+    {
+        return GraphTypeBuilder
+            .<Element, DefaultEdge> directed().allowingMultipleEdges(false)
+            .allowingSelfLoops(true).edgeClass(DefaultEdge.class).weighted(false).buildGraph();
+    }
+}
